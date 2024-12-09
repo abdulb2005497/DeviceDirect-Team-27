09b1dc26453f208b6056740646699c8cdb2c6fdd
MunibShafi-aston <230057117@aston.ac.uk>
2024-12-09 06:36:48 +0000

    fixed issue of having 2 navbars

--- checkoutpage/cart.php
+++ checkoutpage/cart.php
@@ -84,135 +84,13 @@ foreach ($_SESSION['cart'] as $item) {
                     </li>
                     <li class="nav-item"><a class="nav-link" href="../config/logout.php">Logout</a></li>
                 </ul>
-             
+
                 <div class="cart-btn">
                     <a href="../checkoutpage/cart.php"><i class="fas fa-shopping-bag"></i></a>
                 </div>
             </div>
         </div>
     </nav>
-    <title></title>
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/>
-     <!-- bootstrap links -->
-     <link
-     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-     rel="stylesheet"
-     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-     crossorigin="anonymous"
-   />
-   <!-- bootstrap links -->
-   <!-- fonts links -->
-   <link rel="preconnect" href="https://fonts.googleapis.com" />
-   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-   <link
-     href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap"
-     rel="stylesheet"
-   />
- </head>
- <body>
-   <!-- navbar -->
-   <nav class="navbar navbar-expand-lg" id="navbar">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php" id="logo">
-          <img
-            src="../assests/images/device_direct_logo.png"
-            alt="Device Direct Logo"
-            class="logo-img"
-          />
-          <span id="span1">D</span>evice <span>Direct</span></a
-        >
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span
-            ><img src="./assests/images/menu.png" alt="" width="30px"
-          /></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a
-                class="nav-link active"
-                aria-current="page"
-                href="../index.php"
-                >Home</a
-              >
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../productspage/index.php">Shop</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../aboutuspage/aboutus.php">About</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="../contactuspage/contactus.php"
-                >Contact</a
-              >
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Account
-              </a>
-              <ul
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdown"
-                style="background-color: rgb(67 0 86)"
-              >
-                <li>
-                  <a class="dropdown-item" href="../Login_page/login.php"
-                    >Login</a
-                  >
-                </li>
-                <li>
-                  <a class="dropdown-item" href="../Login_page/signup.php"
-                    >SignUp</a
-                  >
-                </li>
-                <li>
-                  <a
-                    class="dropdown-item"
-                    href="../previousorders/previousorders.php"
-                    >Previous Orders</a
-                  >
-                </li>
-              </ul>
-            </li>
-            <li class="nav-item"><a class="nav-link" href="../config/logout.php">Logout</a></li>
-          </ul>
-          <form class="d-flex" id="search">
-            <input
-              class="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button class="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
-          <div class="cart-btn">
-            <a href="../checkoutpage/cart.php"
-              ><i class="fas fa-shopping-bag"></i
-            ></a>
-          </div>
-        </div>
-      </div>
-    </nav>
-    <!-- navbar -->
-
     <div class="container mt-5">
         <h2>Shopping Cart</h2>
 
