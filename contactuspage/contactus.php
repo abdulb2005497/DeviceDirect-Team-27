af55826bfffaa379a65ddff3f47fbad5e0fcbcca
abdulb2005497 <230059052@aston.ac.uk>
2025-02-24 16:00:04 +0000

    Revert "updating session status on contact us"
    This reverts commit 66e602db448da3399a35af520a9954cbeccdfe26.

--- contactuspage/contactus.php
+++ contactuspage/contactus.php
@@ -6,8 +6,9 @@ if (session_status() == PHP_SESSION_NONE) {
 
 
 if (!isset($_SESSION['user_id'])) {
-    header("Location: Login_page/login.php");
-    exit();
+  header("Location: Login_page/login.php");
+  exit();
+
 }
 
 
