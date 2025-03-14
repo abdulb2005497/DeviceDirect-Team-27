84cd354a8ea02cb126e1d9556dcd99d9aa1e2d3a
thelma4 <230060313@aston.ac.uk>
2025-03-14 09:09:53 +0000

    Update update_profile.php

--- update_profile/update_profile.php
+++ update_profile/update_profile.php
@@ -32,4 +32,57 @@ $user = $result->fetch_assoc();
     <div class="container">
         <h1>Update Your Profile</h1>
 
+        <?php if (isset($_SESSION['message'])): ?>
+            <div class="alert <?php echo $_SESSION['message_type']; ?>">
+                <?php 
+                    echo $_SESSION['message']; 
+                    unset($_SESSION['message']);
+                    unset($_SESSION['message_type']);
+                ?>
+            </div>
+        <?php endif; ?>
+        
+        <form action="update_profile.php" method="post">
+            <div class="form-group">
+                <label for="full_name">Full Name</label>
+                <input type="text" id="full_name" name="full_name" value="<?php echo htmlspecialchars($user['full_name']); ?>">
+            </div>
+            
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" value="<?php echo htmlspecialchars($user['email']); ?>">
+            </div>
+            
+            <div class="form-group">
+                <label for="phone">Phone</label>
+                <input type="text" id="phone" name="phone" value="<?php echo htmlspecialchars($user['phone']); ?>">
+            </div>
+            
+            <div class="form-group">
+                <label for="address">Address</label>
+                <textarea id="address" name="address"><?php echo htmlspecialchars($user['address']); ?></textarea>
+            </div>
+            
+            <div class="form-group">
+                <label for="city">City</label>
+                <input type="text" id="city" name="city" value="<?php echo htmlspecialchars($user['city']); ?>">
+            </div>
+            
+            <div class="form-group">
+                <label for="postal_code">Post Code</label>
+                <input type="text" id="post_code" name="post_code" value="<?php echo htmlspecialchars($user['post_code']); ?>">
+            </div>
+            
+            <div class="form-group">
+                <button type="submit" name="update">Update Profile</button>
+            </div>
+        </form>
+        
+        <div class="links">
+            <a href="change_password.php">Change Password</a>
+            <a href="index.php">Back to Homepage</a>
+        </div>
+    </div>
+</body>
+
 </html>
