be734bdb5a97fa50cdc71ea269c51ffde4431903
thelma4 <230060313@aston.ac.uk>
2025-03-14 09:53:55 +0000

    added styling

--- update_profile/update_profile.php
+++ update_profile/update_profile.php
@@ -27,6 +27,9 @@ $user = $result->fetch_assoc();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Profile</title>
     <link rel="stylesheet" href="styles.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
