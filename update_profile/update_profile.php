be16612580e2af701d2351d1e0cfd8ecd0908b71
thelma4 <230060313@aston.ac.uk>
2025-03-14 08:36:33 +0000

    Create update_profile.php
    added page to update user information from user side

---
+++ update_profile/update_profile.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$conn = new mysqli("localhost", "root", "", "devicedirect");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT * FROM users WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+?>
