22b5c3bd99b75fa1677668fca525089d4ae5e35f
MunibShafi-aston <230057117@aston.ac.uk>
2025-03-23 23:54:59 +0000

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,13 +1,18 @@
 <?php
-$host = "localhost";         
-$dbname = "devicedirect"; 
-$username = "root";          
-$password = "";              
+$host = "localhost";
+$dbname = "devicedirect";
+$username = "root";
+$password = "";
+
 try {
-    
-    $pdo = new PDO('mysql:host=localhost;dbname=devicedirect', $username, $password);
+  $pdo = new PDO('mysql:host=localhost;dbname=devicedirect', $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) { header("Location: /login.php"); 
-    exit; 
+} catch (PDOException $e) {
+    if (str_contains($e->getMessage(), "Unknown database")) {
+        die("Error: The database '$dbname' does not exist. Please create it before proceeding. The file can be found in DeviceDirect-Team-27/devicedirect.sql.");
+    } else {
+        header("Location: Login_page/login.php");
+        exit;
+    }
 }
 ?>
