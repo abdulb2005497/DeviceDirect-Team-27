7a6a02e1571aabb8d4e561bdc914e1825a3e5b1a
MunibShafi-aston <230057117@aston.ac.uk>
2025-03-23 22:57:24 +0000

    fixed navbar inconsistency

--- aboutuspage/aboutus.php
+++ aboutuspage/aboutus.php
@@ -22,6 +22,7 @@ $welcome_message = "Welcome, " . htmlspecialchars($_SESSION['first_name']);
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>About Us - Device Direct</title>
     <link rel="stylesheet" href="aboutus.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="../assests/css/style.css?v=<?php echo time(); ?>">
     <link
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
