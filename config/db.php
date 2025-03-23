e154fcdbae222f0b5e4678b90413385a584da16c
Anas <igeanas3@gmail.com>
2025-03-23 16:31:31 +0000

    Update db.php

--- config/db.php
+++ config/db.php
@@ -7,9 +7,7 @@ try {
     
     $pdo = new PDO('mysql:host=localhost;dbname=devicedirect', $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    
-    header("Location: /login.php"); 
+} catch (PDOException $e) { header("Location: /login.php"); 
     exit; 
 }
 ?>
