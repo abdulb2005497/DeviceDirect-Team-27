29635b2510d5ce1ca278d3076a2d4432e0e05d5a
Anas <igeanas3@gmail.com>
2025-03-23 16:30:48 +0000

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -4,6 +4,5 @@ echo "<h1>Welcome, " . htmlspecialchars($_SESSION['first_name']) . "!</h1>";
 if ($_SESSION['role'] === 'admin') {
     echo "<a href='admin_dashboard.php'>Go to Admin Dashboard</a>";
 }
-
 echo "<a href='logout.php'>Logout</a>";
 ?>
